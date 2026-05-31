a01524b41abb5dfb8c536b335b04723362e47f13
OnlyTimmy <tim.harmsen.th@gmail.com>
2026-05-31 13:02:35 +0200

    Test automatische CI CD trigger

--- app/server.js
+++ app/server.js
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
       <body style="font-family: Arial, sans-serif; text-align: center; margin-top: 50px;">
         <h1>IT Landscape CI/CD</h1>
         <p>Deze applicatie draait in een Docker-container.</p>
-        <p>De Docker image wordt automatisch gebouwd via GitHub Actions.</p>
+        <p>De pipeline start automatisch wanneer ik een wijziging push.</p>
       </body>
     </html>
   `);
